d1a6ca0ba313a91ac8e1ed5acdccdf0b6e330893
Barouali-AB <62695088+Barouali-AB@users.noreply.github.com>
2022-06-16 12:07:04 +0200

    Create markov.py

---
+++ markov.py
@@ -0,0 +1,29 @@
+
+def nbr_occur(kgram, text):
+  n = len(text)
+  m = len(kgram)
+  occ = 0
+  for i in range(n-m+1): 
+    if ( text[i:i+m] == kgram):
+      occ += 1
+  return occ
+
+
+def get_characters(kgram, text):
+  n = len(text)
+  m = len(kgram)
+  list_characters = []
+  for i in range(n-m): 
+    if ( text[i:i+m] == kgram):
+        list_characters.append(text[i+m])
+  return list_characters
+
+
+def get_proba_markov(kgram, text):
+  n = len(get_characters(kgram,text))
+  occ = nbr_occur(kgram,text)
+  mydict = {}
+  for i in range(n):
+    mydict[get_characters(kgram,text)[i]] = nbr_occur(kgram+get_characters(kgram,text)[i], text)/occ
+
+  return mydict
