c956f90d2c75f0f0eaeb5fcfc51412adc9992872
Barouali-AB <62695088+Barouali-AB@users.noreply.github.com>
2022-06-24 12:32:59 +0200

    Update markov2.py

--- markov2.py
+++ markov2.py
@@ -5,34 +5,38 @@ class MarkovModel:
 
     def __init__(self, text, k): 
 
-        self.k = k               
+        self.k = k  
+        self.text = text           
         self.tran = defaultdict(float)
         self.alph = list(set(list(text)))
         self.kgrams = defaultdict(int)
         n = len(text)
         for i in range(n-k):
-            self.tran[text[i:i+k],text[i+k]] += 1.
-            self.kgrams[text[i:i+k]] += 1
+            self.tran[str(text[i:i+k]),text[i+k]] += 1.
+            self.kgrams[str(text[i:i+k])] += 1
 
     def order(self):                  
         return self.k
 
     def freq(self, kgram):         
-        assert len(kgram) == self.k    
-        return self.kgrams[kgram]
+        assert len(kgram) == self.k
+        return self.kgrams[str(kgram)]
+    
 
     def freq2(self, kgram, c):   	
         assert len(kgram) == self.k    
-        return self.tran[kgram,c]  
+        return self.tran[str(kgram),c]  
 
-    def rand(self, kgram):            
+    def rand(self, kgram):
+         
         assert len(kgram) == self.k    
-        Z = sum([self.tran[kgram, alph] for alph in self.alph])
-        ele = np.random.choice(self.alph, 1, p=np.array([self.tran[kgram, alph] for alph in self.alph])/Z)
-        p = list(np.array([self.tran[kgram, alph] for alph in self.alph])/Z)
+        Z = sum([self.tran[str(kgram), alph] for alph in self.alph])
+        ele = np.random.choice(self.alph, 1, p=np.array([self.tran[str(kgram), alph] for alph in self.alph])/Z)
+        p = list(np.array([self.tran[str(kgram), alph] for alph in self.alph])/Z)
         i = self.alph.index(ele[0])
         return ele[0], p[i]
-        
+      
+           
     def gen(self, kgram, T):          
         assert len(kgram) == self.k    
         str = kgram
@@ -43,3 +47,5 @@ class MarkovModel:
              kgram = kgram[1:] + c     
              str += c			 
         return str, proba
+
+    
