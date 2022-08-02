506961b8812ce9fb68003a7357e1ad10524d1dfd
Barouali-AB <62695088+Barouali-AB@users.noreply.github.com>
2022-08-03 00:02:57 +0200

    Update markov.py

--- markov.py
+++ markov.py
@@ -9,16 +9,6 @@ def nbr_occur(kgram, text):
   return occ
 
 
-def get_characters(kgram, text):
-  n = len(text)
-  m = len(kgram)
-  list_characters = []
-  for i in range(n-m): 
-    if ( text[i:i+m] == kgram and text[i+m] not in list_characters):
-        list_characters.append(text[i+m])
-  return list_characters
-
-
 def get_proba_markov(kgram, text):
   n = len(get_characters(kgram,text))
   occ = nbr_occur(kgram,text)
